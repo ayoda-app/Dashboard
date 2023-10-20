85bded29ce517fb693ba3812dec726517eaa1f07
John Khachian <jaksawesome10@gmail.com>
2023-10-20 15:31:29 -0400

    added underline to navigation sections

--- src/components/Dashboard/Navbar.vue
+++ src/components/Dashboard/Navbar.vue
@@ -13,19 +13,31 @@ export default {
     >
         <router-link to="/dashboard/predictions" class="subtitle m-0">
             <font-awesome-icon :icon="['fas', 'chart-line']" />
-            <label class="ml-3">Predictions</label>
+            <label
+                class="ml-3"
+                :class="{ 'is-underlined': $route.params.section === 'predictions' }"
+            >Predictions</label>
         </router-link>
         <router-link to="/dashboard/insights" class="subtitle m-0">
             <font-awesome-icon :icon="['fas', 'magnifying-glass-chart']" />
-            <label class="ml-3">Insights</label>
+            <label
+                class="ml-3"
+                :class="{ 'is-underlined': $route.params.section === 'insights' }"
+            >Insights</label>
         </router-link>
         <router-link to="/dashboard/orders" class="subtitle m-0">
             <font-awesome-icon :icon="['fas', 'receipt']" />
-            <label class="ml-3">Orders</label>
+            <label
+                class="ml-3"
+                :class="{ 'is-underlined': $route.params.section === 'orders' }"
+            >Orders</label>
         </router-link>
         <router-link to="/dashboard/past-orders" class="subtitle m-0">
             <font-awesome-icon :icon="['fas', 'box-archive']" />
-            <label class="ml-3">Past Orders</label>
+            <label
+                class="ml-3"
+                :class="{ 'is-underlined': $route.params.section === 'past-orders' }"
+            >Past Orders</label>
         </router-link>
     </nav>
 </template>
