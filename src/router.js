8267a34f0d5e5ea850603a430032385be8270267
John Khachian <jaksawesome10@gmail.com>
2023-10-29 15:50:22 -0400

    changed routing code

--- src/router.js
+++ src/router.js
@@ -33,9 +33,10 @@ const routes = [
 
 const router = createRouter({
   history: (
-    process.env.NODE_ENV === "production"
-        ? createWebHistory("/Dashboard/")
-        : createWebHistory()
+    // process.env.NODE_ENV === "production"
+    //     ? createWebHistory("/Dashboard/")
+        // : createWebHistory("/")
+        createWebHistory("/")
   ),
   routes,
 })
